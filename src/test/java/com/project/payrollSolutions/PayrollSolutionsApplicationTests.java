b6bf8a59005085214d235a7a2a1269fd07b3bad8
Arthur Santana <arthurgiannonisantana@yahoo.com.br>
2023-09-25 19:19:36 -0300

    feat: first commit and creation of environment variables

---
+++ src/test/java/com/project/payrollSolutions/PayrollSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.payrollSolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
