d7f0d88938196b92d15125cfe1a17bc7a6d5e2bc
Arthur Santana <arthurgiannonisantana@yahoo.com.br>
2023-09-26 19:36:13 -0300

    feat: creating all repositories for my Entity files

---
+++ src/main/java/com/project/payrollSolutions/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.project.payrollSolutions.repository;
+
+import com.project.payrollSolutions.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
