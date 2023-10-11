dbcc98dd70962901bbb1e2f8ff9ba42ae98de2a1
Arthur Santana <arthurgiannonisantana@yahoo.com.br>
2023-10-11 13:31:32 -0300

    feat: creating service and methods for FrequencyControl

--- src/main/java/com/project/payrollSolutions/service/FrequencyControlService.java
+++ src/main/java/com/project/payrollSolutions/service/FrequencyControlService.java
@@ -1,9 +1,12 @@
 package com.project.payrollSolutions.service;
 
+import com.project.payrollSolutions.dto.FrequencyControlRequestDTO;
 import com.project.payrollSolutions.model.FrequencyControl;
 import com.project.payrollSolutions.repository.FrequencyControlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class FrequencyControlService {
@@ -15,7 +18,37 @@ public class FrequencyControlService {
         this.frequencyControlRepository = frequencyControlRepository;
     }
 
-//    public FrequencyControl createFrequencyControl() {
-//
-//    }
+    public FrequencyControl createFrequencyControl(FrequencyControlRequestDTO frequencyControlRequestDTO) {
+        FrequencyControl frequencyControl = frequencyControlRequestDTO.transformToFrequencyControl();
+
+        frequencyControl = frequencyControlRepository.save(frequencyControl);
+
+        return frequencyControl;
+    }
+
+    public FrequencyControl updateFrequencyControl(FrequencyControlRequestDTO frequencyControlRequestDTO) {
+        Optional<FrequencyControl> frequencyControlOptional = frequencyControlRepository.findById(frequencyControlRequestDTO.getId());
+
+        if (frequencyControlOptional.isPresent()) {
+            FrequencyControl frequencyControl = frequencyControlRequestDTO.transformToFrequencyControl();
+
+            frequencyControl = frequencyControlRepository.save(frequencyControl);
+
+            return frequencyControl;
+        } else {
+            throw new RuntimeException("FrequencyControl by id " + frequencyControlRequestDTO.getId() + " was not found");
+        }
+    }
+
+    public void deleteFrequencyControl(Long id) {
+        Optional<FrequencyControl> frequencyControlOptional = frequencyControlRepository.findById(id);
+
+        if (frequencyControlOptional.isPresent()) {
+            FrequencyControl frequencyControl = frequencyControlOptional.get();
+
+            frequencyControlRepository.delete(frequencyControl);
+        } else {
+            throw new RuntimeException("FrequencyControl by id " + id + " was not found");
+        }
+    }
 }
