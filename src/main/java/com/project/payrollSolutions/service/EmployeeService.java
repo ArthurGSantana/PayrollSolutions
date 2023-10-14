be63fe76b3bc8410ad116c49e23ecd2c6e9c908e
Arthur Santana <arthurgiannonisantana@yahoo.com.br>
2023-10-14 10:34:56 -0300

    feat: creating new service for Address entity

--- src/main/java/com/project/payrollSolutions/service/EmployeeService.java
+++ src/main/java/com/project/payrollSolutions/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.project.payrollSolutions.service;
 
 import com.project.payrollSolutions.dto.EmployeeRequestDTO;
+import com.project.payrollSolutions.exceptionhandler.NotFoundException;
 import com.project.payrollSolutions.model.Address;
 import com.project.payrollSolutions.model.Employee;
 import com.project.payrollSolutions.repository.AddressRepository;
@@ -15,18 +16,27 @@ import java.util.Optional;
 @Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
-    private final AddressRepository addressRepository;
+    private final AddressService addressService;
 
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository, AddressRepository addressRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository, AddressService addressService) {
         this.employeeRepository = employeeRepository;
-        this.addressRepository = addressRepository;
+        this.addressService = addressService;
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("EmployeeId " + id + " was not found"));
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
     }
 
     @Transactional
     public Employee createEmployee(EmployeeRequestDTO employee) {
         Address address = employee.getAddress().transformToAddress();
-        address = addressRepository.save(address);
+        address = addressService.createAddress(address);
 
         Employee newEmployee = employee.transformToEmployee();
         newEmployee.setAddress(address);
@@ -35,74 +45,22 @@ public class EmployeeService {
     }
 
     @Transactional
-    public Employee updateEmployee(Employee employee) {
-        Optional<Employee> employeeOptional = employeeRepository.findById(employee.getId());
+    public void updateEmployee(EmployeeRequestDTO employeeRequestDTO) {
+        findEmployeeById(employeeRequestDTO.getId());
+        Employee employee = employeeRequestDTO.transformToEmployee();
 
-        if (employeeOptional.isPresent()) {
+        employeeRepository.save(employee);
 
-            Employee newEmployee = getEmployee(employee, employeeOptional.get());
-            newEmployee = employeeRepository.save(newEmployee);
-
-            if (newEmployee.getAddress() != null) {
-                Address newAddress = getAddress(employee.getAddress(), new Address());
-                newAddress = addressRepository.save(newAddress);
-                newEmployee.setAddress(newAddress);
-                return newEmployee;
-            } else {
-                throw new RuntimeException("Address by id " + employee.getAddress().getId() + " was not found");
-            }
-        } else {
-            throw new RuntimeException("Employee by id " + employee.getId() + " was not found");
-        }
-    }
-
-    private Employee getEmployee(Employee employee, Employee newEmployee) {
-        newEmployee.setName(employee.getName());
-        newEmployee.setEmail(employee.getEmail());
-        newEmployee.setDocument(employee.getDocument());
-        newEmployee.setJobTitle(employee.getJobTitle());
-        newEmployee.setBaseSalary(employee.getBaseSalary());
-        newEmployee.setPhone(employee.getPhone());
-        newEmployee.setBirthDate(employee.getBirthDate());
-        newEmployee.setAddress(employee.getAddress());
-        return newEmployee;
-    }
-
-    private Address getAddress(Address address, Address newAddress) {
-        newAddress.setStreetAddress(address.getStreetAddress());
-        newAddress.setCity(address.getCity());
-        newAddress.setPostalCode(address.getPostalCode());
-        newAddress.setNeighborhood(address.getNeighborhood());
-        newAddress.setHouseNumber(address.getHouseNumber());
-        return newAddress;
+        Address address = employee.getAddress();
+        addressService.updateAddress(address);
     }
 
     @Transactional
     public void deleteEmployee(Long id) {
-        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+        Employee employee = findEmployeeById(id);
+        employeeRepository.delete(employee);
 
-        if (employeeOptional.isPresent()) {
-            Employee employee = employeeOptional.get();
-            employeeRepository.delete(employee);
-
-            if (employee.getAddress() != null) {
-                Address address = employee.getAddress();
-                addressRepository.delete(address);
-            } else {
-                throw new RuntimeException("Address by employee id " + employee.getId() + " was not found");
-            }
-        } else {
-            throw new RuntimeException("Employee by id " + id + " was not found");
-        }
-    }
-
-    public Employee findEmployeeById(Long id) {
-        return employeeRepository.findById(id)
-                .orElseThrow(() -> null);
-
-    }
-
-    public List<Employee> findAllEmployees() {
-        return employeeRepository.findAll();
+        Address address = employee.getAddress();
+        addressService.deleteAddress(address);
     }
 }
